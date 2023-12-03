88ce1b7523b8d5ec2d4540948f717b6a65c3314c
Shubham Tank <tankshubham3322@gmail.com>
2023-12-04 00:03:59 +0530

    Add API for updating issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { ErrorMessage, SkeletonTheme, Spinner } from '@/app/components';
-import { createIssueSchema } from '@/app/validationSchemas';
+import { issueSchema } from '@/app/validationSchemas';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Issue } from '@prisma/client';
 import { Button, Callout, TextField } from '@radix-ui/themes';
@@ -26,11 +26,11 @@ const SimpleMDE = dynamic(
   }
 )
 
-type IssueFormData = z.infer<typeof createIssueSchema>
+type IssueFormData = z.infer<typeof issueSchema>
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
   const { register, control, handleSubmit, formState: { errors } } = useForm<IssueFormData>({
-    resolver: zodResolver(createIssueSchema)
+    resolver: zodResolver(issueSchema)
   })
   const router = useRouter()
 
