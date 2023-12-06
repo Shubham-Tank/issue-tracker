8691908bdb80b1f92eded6a553ffe284b8c430aa
Shubham Tank <tankshubham3322@gmail.com>
2023-12-06 23:52:09 +0530

    Add Login dropdown with avatar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,12 +5,29 @@ import { usePathname } from 'next/navigation';
 import React from 'react'
 import { FaBug } from "react-icons/fa";
 import classNames from 'classnames';
-import { Box, Container, Flex } from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 import { useSession } from 'next-auth/react'
 
 const NavBar = () => {
+  return (
+    <>
+      <nav className='px-6 py-4 items-center text-white bg-slate-800'>
+        <Flex justify="between">
+          <Flex align='center' gap="5">
+            <Link href="/"><FaBug /></Link>
+            <NavLinks />
+          </Flex>
+          <Box>
+            <AuthStatus />
+          </Box>
+        </Flex>
+      </nav >
+    </>
+  )
+}
+
+const NavLinks = () => {
   const currentPath = usePathname()
-  const { status, data: session } = useSession()
 
   const links = [
     { label: "Dashboard", href: '/' },
@@ -18,36 +35,55 @@ const NavBar = () => {
   ]
 
   return (
-    <>
-      <nav className='px-6 py-5 items-center text-white bg-slate-800'>
-        <Flex justify="between">
-          <Flex align='center' gap="5">
-            <Link href="/"><FaBug /></Link>
-            <ul className='flex gap-6'>
-              {
-                links.map(({ label, href }) => (
-                  <li key={href}>
-                    <Link
-                      href={href}
-                      className={classNames({
-                        'border-cyan-600 rounded-lg transition-all': true,
-                        'border-b-4 p-2': href === currentPath,
-                        'border-b-0 p-1': href !== currentPath
-                      })}>
-                      {label}
-                    </Link>
-                  </li>
-                ))
-              }
-            </ul>
-          </Flex>
-          <Box>
-            {status === 'authenticated' && <Link href="/api/auth/signout">Log out</Link>}
-            {status === 'unauthenticated' && <Link href="/api/auth/signin">Login</Link>}
-          </Box>
-        </Flex>
-      </nav >
-    </>
+    <ul className='flex gap-6'>
+      {
+        links.map(({ label, href }) => (
+          <li key={href}>
+            <Link
+              href={href}
+              className={classNames({
+                'border-cyan-600 rounded-lg transition-all': true,
+                'border-b-4 p-2': href === currentPath,
+                'border-b-0 p-1': href !== currentPath
+              })}>
+              {label}
+            </Link>
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession()
+
+  if (status === 'loading') return null
+
+  if (status === 'unauthenticated') return <Link href="/api/auth/signin">Login</Link>
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <Avatar
+          src={session!.user!.image!}
+          fallback="?"
+          size="2"
+          radius='full'
+          className='cursor-pointer'
+        />
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <DropdownMenu.Label>
+          <Text>
+            {session!.user!.email}
+          </Text>
+        </DropdownMenu.Label>
+        <DropdownMenu.Item>
+          <Link href="/api/auth/signout">Log out</Link>
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   )
 }
 
