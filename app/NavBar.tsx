e57cc307c9c7eb131ed010fa551fa12232b938f4
Shubham Tank <tankshubham3322@gmail.com>
2023-12-06 23:18:53 +0530

    Update navbar layout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation';
 import React from 'react'
 import { FaBug } from "react-icons/fa";
 import classNames from 'classnames';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 import { useSession } from 'next-auth/react'
 
 const NavBar = () => {
@@ -19,29 +19,33 @@ const NavBar = () => {
 
   return (
     <>
-      <nav className='flex space-x-6 px-6 py-5 items-center text-white bg-slate-800'>
-        <Link href="/"><FaBug /></Link>
-        <ul className='flex gap-6'>
-          {
-            links.map(({ label, href }) => (
-              <li key={href}>
-                <Link
-                  href={href}
-                  className={classNames({
-                    'border-cyan-600 rounded-lg transition-all': true,
-                    'border-b-4 p-2': href === currentPath,
-                    'border-b-0 p-1': href !== currentPath
-                  })}>
-                  {label}
-                </Link>
-              </li>
-            ))
-          }
-        </ul>
-        <Box>
-          {status === 'authenticated' && <Link href="/api/auth/signout">Log out</Link>}
-          {status === 'unauthenticated' && <Link href="/api/auth/signin">Login</Link>}
-        </Box>
+      <nav className='px-6 py-5 items-center text-white bg-slate-800'>
+        <Flex justify="between">
+          <Flex align='center' gap="5">
+            <Link href="/"><FaBug /></Link>
+            <ul className='flex gap-6'>
+              {
+                links.map(({ label, href }) => (
+                  <li key={href}>
+                    <Link
+                      href={href}
+                      className={classNames({
+                        'border-cyan-600 rounded-lg transition-all': true,
+                        'border-b-4 p-2': href === currentPath,
+                        'border-b-0 p-1': href !== currentPath
+                      })}>
+                      {label}
+                    </Link>
+                  </li>
+                ))
+              }
+            </ul>
+          </Flex>
+          <Box>
+            {status === 'authenticated' && <Link href="/api/auth/signout">Log out</Link>}
+            {status === 'unauthenticated' && <Link href="/api/auth/signin">Login</Link>}
+          </Box>
+        </Flex>
       </nav >
     </>
   )
