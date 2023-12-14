3cb1ae7efa5605f0c64cf5a45dc6c1ec87575954
Shubham Tank <tankshubham3322@gmail.com>
2023-12-14 18:29:22 +0530

    Add Signin with credentials functionality

--- app/auth/authOptions.tsx
+++ app/auth/authOptions.tsx
@@ -2,10 +2,33 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from "@/prisma/client"
 import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from 'bcrypt'
 
 const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "user@example.com" },
+        password: { label: "Password", type: "password", placeholder: "Password" }
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) return null
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email }
+        })
+
+        if (!user) return null
+
+        const passwordsMatch = await bcrypt.compare(credentials.password, user.password || '')
+
+        return passwordsMatch ? user : null
+
+      }
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!
