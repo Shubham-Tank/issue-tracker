9f34469598ea7c7148c96e330fbc23d96160ca44
Shubham Tank <tankshubham3322@gmail.com>
2023-12-19 17:20:47 +0530

    Add api to check available slug for project

---
+++ app/api/projects/available/[slug]/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from '@/prisma/client'
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
+
+export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
+
+  const session = await getServerSession(authOptions)
+  if (!session) return NextResponse.json({}, { status: 401 })
+
+  const { slug } = params
+
+  const project = await prisma.project.findUnique({
+    where: { slug }
+  });
+
+  if (project)
+    return NextResponse.json({ available: false }, { status: 409 });
+
+  return NextResponse.json({ available: true }, { status: 200 });
+}
