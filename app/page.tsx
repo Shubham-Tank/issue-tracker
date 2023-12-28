3ac0587035a0fae19d2be751e7923da43855db9a
Shubham Tank <tankshubham3322@gmail.com>
2023-12-28 17:42:45 +0530

    Remove IssueBoard from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,15 @@ import prisma from '@/prisma/client'
 import { Container, Flex, Grid } from '@radix-ui/themes'
 import { Metadata } from 'next'
 import { getServerSession } from 'next-auth'
-import lazyload from 'next/dynamic'
 import IssueChart from './IssueChart'
 import IssueSummary from './IssueSummary'
 import LatestIssues from './LatestIssues'
 import ProjectList from './ProjectList'
 import authOptions from './auth/authOptions'
 
-const IssueBoard = lazyload(
-  () => import('./components/IssueBoard'),
-  {
-    ssr: false
-  }
-)
-
 export default async function Home() {
 
   const session = await getServerSession(authOptions)
-
-  const issues = await prisma.issue.findMany({
-    select: {
-      id: true,
-      title: true,
-      status: true
-    }
-  })
 
   const open = await prisma.issue.count({ where: { status: 'OPEN' } })
   const inProgress = await prisma.issue.count({ where: { status: 'IN_PROGRESS' } })
@@ -37,7 +21,6 @@ export default async function Home() {
   return (
     <Container className='py-7'>
       <ProjectList session={session} />
-      <IssueBoard issues={issues} />
       <Grid columns={{ initial: '1', md: '2' }} gap="5">
         <Flex direction="column" gap="5">
           <IssueSummary {...stats} />
