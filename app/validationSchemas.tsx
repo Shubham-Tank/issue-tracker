dbc689e74687b15b50c08b3a410971bdb9cfe68c
Shubham Tank <tankshubham3322@gmail.com>
2023-12-28 23:47:33 +0530

    Update patch issue api for updating status

--- app/validationSchemas.tsx
+++ app/validationSchemas.tsx
@@ -8,6 +8,7 @@ export const issueSchema = z.object({
 export const patchIssueSchema = z.object({
   title: z.string().min(1, 'Title is required').max(255).optional(),
   description: z.string().min(1, 'Description is required').max(65535).optional(),
+  status: z.enum(['OPEN', 'IN_PROGRESS', 'CLOSED']).optional(),
   assignedToUserId: z.string().min(1, 'assignedToUserId is required').max(255).optional().nullable()
 });
 
